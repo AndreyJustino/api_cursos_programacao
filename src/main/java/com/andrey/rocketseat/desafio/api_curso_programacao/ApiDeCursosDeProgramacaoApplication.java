472ea340b9662afd6e8f03f7f05b189881299829
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-22 19:41:58 -0300

    first commit

---
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/ApiDeCursosDeProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeCursosDeProgramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeCursosDeProgramacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/ApiDeCursosDeProgramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeCursosDeProgramacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
