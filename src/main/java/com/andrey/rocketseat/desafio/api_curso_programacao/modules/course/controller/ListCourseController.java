88617dcf8a3d82d12da775b438ec9d302c7cce33
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-23 17:16:39 -0300

    feat: controller para listar todos os cursos

---
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/controller/ListCourseController.java
@@ -0,0 +1,24 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.controller;
+
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.CourseEntity;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.service.ListCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cursos")
+public class ListCourseController {
+
+    @Autowired
+    private ListCourseService listCourseService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<CourseEntity>> listCourseController(){
+        return ResponseEntity.ok().body(listCourseService.execute());
+    }
+}
