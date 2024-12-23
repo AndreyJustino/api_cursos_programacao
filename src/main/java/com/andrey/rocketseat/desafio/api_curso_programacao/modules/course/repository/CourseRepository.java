ab2a51742c9d03a754817d9d2d6972c1bf135222
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-23 17:03:18 -0300

    feat: adicionando metodo pra encontrar curso por nome e categoria na repository

--- src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/repository/CourseRepository.java
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/repository/CourseRepository.java
@@ -3,8 +3,9 @@ package com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.repos
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
-
+    Optional<CourseEntity> findByNameAndCategory(String name, String category);
 }
