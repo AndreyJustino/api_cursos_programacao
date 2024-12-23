8e5f3765ee05c6b6d44032a3c9da1e46f0bb27f1
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-23 19:10:18 -0300

    feat: controller para atualizacao de dados

---
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/controller/UpdateCourseController.java
@@ -0,0 +1,39 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.controller;
+
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.CourseEntity;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.dto.MessageReturnDTO;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.dto.UpdateCourseDTO;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.service.UpdateCourseService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cursos")
+public class UpdateCourseController {
+    @Autowired
+    private UpdateCourseService updateCourseService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateCourseController(@PathVariable UUID id, @Valid @RequestBody UpdateCourseDTO updateCourseDTO){
+        try{
+
+            CourseEntity result = this.updateCourseService.execute(id, updateCourseDTO);
+
+            MessageReturnDTO messageReturnDTO = MessageReturnDTO.builder()
+                    .message("Usuario atualizado com sucesso!")
+                    .object(result)
+                    .build();
+
+            return ResponseEntity.ok().body(messageReturnDTO);
+
+        } catch (Exception e) {
+            e.getMessage();
+
+            throw new RuntimeException(e);
+        }
+    }
+}
