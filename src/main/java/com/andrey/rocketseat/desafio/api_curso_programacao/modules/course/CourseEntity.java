bc1b196bf4f8a6edf3b19a86f0c87f70bf720d0e
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-26 21:13:52 -0300

    feat: add "schema" com swagger as entidades

--- src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/CourseEntity.java
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/CourseEntity.java
@@ -3,6 +3,7 @@ package com.andrey.rocketseat.desafio.api_curso_programacao.modules.course;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,13 +22,16 @@ public class CourseEntity {
     private UUID id;
 
     @NotBlank(message = "O campo (nome) não deve estar vazio")
+    @Schema(example = "Curso de Java", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
 
     @NotBlank(message = "O campo (categoria) não deve estar vazio")
+    @Schema(example = "Back-end", requiredMode = Schema.RequiredMode.REQUIRED)
     private String category;
 
     @NotNull(message = "O campo (status) não deve estar vazio")
     @Enumerated(EnumType.STRING)
+    @Schema(example = "ATIVADO", requiredMode = Schema.RequiredMode.REQUIRED)
     private Status status;
 
     @CreationTimestamp
--- src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/dto/UpdateCourseDTO.java
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/course/dto/UpdateCourseDTO.java
@@ -1,6 +1,7 @@
 package com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.dto;
 
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
@@ -17,12 +18,15 @@ import lombok.NoArgsConstructor;
 public class UpdateCourseDTO {
 
     @NotBlank(message = "O campo (nome) não deve estar vazio")
+    @Schema(example = "Curso de JavaScript", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
 
     @NotBlank(message = "O campo (categoria) não deve estar vazio")
+    @Schema(example = "Front-End", requiredMode = Schema.RequiredMode.REQUIRED)
     private String category;
 
     @NotNull(message = "O campo (status) não deve estar vazio")
     @Enumerated(EnumType.STRING)
+    @Schema(example = "DESATIVADO", requiredMode = Schema.RequiredMode.REQUIRED)
     private Status status;
 }
