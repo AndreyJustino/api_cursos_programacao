96af81bacb67dbcc9bc06018af557192e4052021
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-01 18:29:51 -0300

    feat: add entidade de student

---
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/student/StudentEntity.java
@@ -0,0 +1,55 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao.modules.student;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "student")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+public class StudentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "O campo (name) não pode estar vazio")
+    @Schema(example = "Andrey Justino", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @NotBlank(message = "O campo (username) não pode estar vazio")
+    @Schema(example = "andreyJustino", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String username;
+
+    @Email(message = "O campo (email) precisa de um e-mail valido")
+    @Schema(example = "andrey@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String email;
+
+    @Length(max = 16, min = 8, message = "A senha deve ter entre 8 e 16 carateres")
+    @Schema(example = "12345678", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
