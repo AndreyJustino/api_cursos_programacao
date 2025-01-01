d04ec288bba3e7310395cf36233f396ac4a2a397
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-01 18:39:29 -0300

    feat: add repository de student

---
+++ src/main/java/com/andrey/rocketseat/desafio/api_curso_programacao/modules/student/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.repository;
+
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
+    Optional<StudentEntity> findByEmailAndPassword(String email, String password);
+}
