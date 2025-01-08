132fbcb4fd9d8d3f68cc9ed56695e306970121bc
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-08 20:25:24 -0300

    feat: teste unitario que verifica se o curso foi atualizado corretamente

--- src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/course/service/UpdateCourseServiceTest.java
+++ src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/course/service/UpdateCourseServiceTest.java
@@ -1,5 +1,8 @@
 package com.andrey.rocketseat.desafio.api_curso_programacao.module.course.service;
 
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.CourseEntity;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.Status;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.dto.UpdateCourseDTO;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.repository.CourseRepository;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.service.UpdateCourseService;
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdateCourseServiceTest {
@@ -31,5 +37,25 @@ public class UpdateCourseServiceTest {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
             assertEquals("Curso não encontrado", e.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("Should return course entity update")
+    public void should_return_course_entity_update(){
+
+        UUID id = UUID.randomUUID();
+
+        UpdateCourseDTO updateCourseDTO = new UpdateCourseDTO();
+
+        updateCourseDTO.setName("TEST NAME");
+        updateCourseDTO.setStatus(Status.ATIVADO);
+        updateCourseDTO.setCategory("TEST CATEGORY");
+
+        when(this.courseRepository.findById(id)).thenReturn(Optional.of(new CourseEntity()));
+        when(this.courseRepository.save(any(CourseEntity.class))).thenReturn(new CourseEntity());
+
+        CourseEntity result = this.updateCourseService.execute(id, updateCourseDTO);
+
+        assertThat(result).isInstanceOf(CourseEntity.class);
     }
 }
