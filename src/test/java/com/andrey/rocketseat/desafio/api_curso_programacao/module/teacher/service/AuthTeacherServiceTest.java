da9fadc8ad02167832047201081f468e76623bd3
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-09 18:42:33 -0300

    feat: teste unitario que diz que a senha esta incorreta

--- src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/teacher/service/AuthTeacherServiceTest.java
+++ src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/teacher/service/AuthTeacherServiceTest.java
@@ -1,6 +1,7 @@
 package com.andrey.rocketseat.desafio.api_curso_programacao.module.teacher.service;
 
 import com.andrey.rocketseat.desafio.api_curso_programacao.exception.TeacherNotFoundException;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.teacher.TeacherEntity;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.teacher.dto.AuthTeacherRequestDTO;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.teacher.repository.TeacherRepository;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.teacher.service.AuthTeacherService;
@@ -12,7 +13,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -36,5 +43,21 @@ public class AuthTeacherServiceTest {
         }
     }
 
+    @Test
+    @DisplayName("should_return_that_the_password_is_incorrect")
+    public void should_return_that_the_password_is_incorrect(){
+        AuthTeacherRequestDTO authTeacherRequestDTO = new AuthTeacherRequestDTO();
+        authTeacherRequestDTO.setEmail("emailTest@mail.com");
 
+        when(this.teacherRepository.findByEmail(authTeacherRequestDTO.getEmail())).thenReturn(Optional.of(new TeacherEntity()));
+
+        when(this.passwordEncoder.matches(any(), any())).thenReturn(false);
+
+        try {
+            this.authTeacherService.execute(authTeacherRequestDTO);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+            assertEquals("A senha fornecida não esta correta.", e.getMessage());
+        }
+    }
 }
