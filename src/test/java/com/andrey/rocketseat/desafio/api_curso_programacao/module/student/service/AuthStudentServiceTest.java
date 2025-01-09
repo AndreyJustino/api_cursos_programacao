c61dece639bac678694ed14cf911345196c27749
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-09 16:59:11 -0300

    feat: teste unitario que verifica se a autenticacao foi bem sucedidae

--- src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/student/service/AuthStudentServiceTest.java
+++ src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/student/service/AuthStudentServiceTest.java
@@ -2,10 +2,13 @@ package com.andrey.rocketseat.desafio.api_curso_programacao.module.student.servi
 
 import com.andrey.rocketseat.desafio.api_curso_programacao.exception.StudentNotFoundException;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.StudentEntity;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.dto.AuthStudentResponseDTO;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.dto.AuthStundentDTO;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.repository.StudentRepository;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.student.service.AuthStudentService;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.security.auth.message.AuthException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,8 +16,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,6 +36,12 @@ public class AuthStudentServiceTest {
 
     @InjectMocks
     private AuthStudentService authStudentService;
+
+    @BeforeEach
+    void setUp(){
+        //configurando valor do secret (injetando valores em atributos privados)
+        ReflectionTestUtils.setField(authStudentService, "secretToken", "secretTokenJwt");
+    }
 
     @Test
     @DisplayName("Should return student not found")
@@ -64,4 +75,27 @@ public class AuthStudentServiceTest {
             assertEquals("Senha invalida", e.getMessage());
         }
     }
+
+    @Test
+    @DisplayName("Should return DTO with token and time to expire")
+    public void should_return_dto_with_token_and_time_to_expire() throws AuthException {
+        AuthStundentDTO authStundentDTO = new AuthStundentDTO();
+        authStundentDTO.setEmail("emailTeste@mail.com");
+
+        StudentEntity student = new StudentEntity();
+        student.setId(UUID.randomUUID());
+
+        when(this.studentRepository.findByEmail(authStundentDTO.getEmail())).thenReturn(Optional.of(student));
+
+        when(this.passwordEncoder.matches(any(),any()))
+                .thenReturn(true);
+
+        AuthStudentResponseDTO result = this.authStudentService.execute(authStundentDTO);
+
+        assertThat(result).isInstanceOf(AuthStudentResponseDTO.class);
+        assertThat(result).isNotNull();
+        assertThat(result.getToken()).isNotNull();
+        assertThat(result.getExpireAt()).isNotNull();
+    }
+
 }
