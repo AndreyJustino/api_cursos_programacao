defc04e7511a0ea258c182934be6bac4e1807c0e
AndreyJustino <andreyjustino2020@gmail.com>
2025-01-08 21:16:21 -0300

    feat: teste unitario que diz que o id nao foi encontrado

--- src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/course/service/UpdatePartilServiceTest.java
+++ src/test/java/com/andrey/rocketseat/desafio/api_curso_programacao/module/course/service/UpdatePartilServiceTest.java
@@ -1,5 +1,8 @@
 package com.andrey.rocketseat.desafio.api_curso_programacao.module.course.service;
 
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.CourseEntity;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.Status;
+import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.dto.UpdateCourseDTO;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.repository.CourseRepository;
 import com.andrey.rocketseat.desafio.api_curso_programacao.modules.course.service.UpdatePartilService;
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdatePartilServiceTest {
@@ -31,5 +37,21 @@ public class UpdatePartilServiceTest {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
             assertEquals("ID fornecido, não encontrado", e.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("Should return course entity update")
+    public void should_return_course_entity_update(){
+
+        UUID id = UUID.randomUUID();
+
+        when(this.courseRepository.findById(id)).thenReturn(Optional.of(new CourseEntity()));
+
+
+        when(this.courseRepository.save(any(CourseEntity.class))).thenReturn(new CourseEntity());
+
+        CourseEntity result = this.updatePartilService.updatePartilService(id, Status.ATIVADO);
+
+        assertThat(result).isInstanceOf(CourseEntity.class);
     }
 }
